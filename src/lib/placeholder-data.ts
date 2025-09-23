4e0a882905a7548e54d267623578cd1214c839f9
Rubin Imeri <rubinimeri6@gmail.com>
2025-09-23 18:08:25 +0200

    Add placeholder-data.ts & sidebar data

---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,67 @@
+export const sidebarData = [
+  {
+    title: "Reseller Area",
+    links: [
+      {
+        title: "Dashboard",
+        url: "/dashboard",
+        iconUrl: "/dashboard/reseller-area/dashboard-icon.png"
+      },
+      {
+        title: "Purchase eSIM",
+        url: "/purchase-esim",
+        iconUrl: "/dashboard/reseller-area/purchase-esim.png"
+      },
+      {
+        title: "eSIM Orders",
+        url: "/esim-orders",
+        iconUrl: "/dashboard/esim-orders.png"
+      },
+      {
+        title: "Transactions",
+        url: "/transactions",
+        iconUrl: "/dashboard/transactions.png"
+      },
+      {
+        title: "My Profile",
+        url: "/profile",
+        iconUrl: "/dashboard/reseller-area/profile.png"
+      }
+    ]
+  },
+  {
+    title: "Admin Area",
+    links: [
+      {
+        title: "Admin Dashboard",
+        url: "/admin-dashboard",
+        iconUrl: "/dashboard/admin-area/admin-dashboard-icon.png"
+      },
+      {
+        title: "Resellers",
+        url: "/resellers",
+        iconUrl: "/dashboard/admin-area/resellers.png"
+      },
+      {
+        title: "eSIM Orders",
+        url: "/esim-orders",
+        iconUrl: "/dashboard/esim-orders.png"
+      },
+      {
+        title: "Transactions",
+        url: "/transactions",
+        iconUrl: "/dashboard/transactions.png"
+      },
+      {
+        title: "Mail Templates",
+        url: "/mail-templates",
+        iconUrl: "/dashboard/admin-area/mail-templates.png"
+      },
+      {
+        title: "Settings",
+        url: "/settings",
+        iconUrl: "/dashboard/admin-area/settings.png"
+      }
+    ]
+  }
+]
