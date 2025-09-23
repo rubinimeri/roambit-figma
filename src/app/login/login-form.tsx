df51d7b0a7e1facdc328e3892e5473491526da31
Rubin Imeri <rubinimeri6@gmail.com>
2025-09-23 14:12:00 +0200

    Add login-form.tsx functionality

---
+++ src/app/login/login-form.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import React, {FormEvent, useState} from 'react';
+import Input from "@/components/ui/input";
+import {redirect} from "next/navigation";
+import {sleep} from "@/lib/utils";
+
+const exampleData = {
+  email: "johndoe@gmail.com",
+  password: "123456",
+}
+
+function LoginForm() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setLoading(true);
+    await sleep(1000);
+
+    if (email === exampleData.email && password === exampleData.password) {
+      redirect('/')
+    }
+
+    setLoading(false);
+    setError("Incorrect email or password!")
+  }
+  return (
+    <form className={"grid gap-8"} onSubmit={onSubmit}>
+      <div className={"grid gap-4"}>
+        <div className={"relative"}>
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            name={"email"}
+            placeholder={"Email"}/>
+          <img
+            src="/login/email.png"
+            alt="email"
+            aria-hidden="true"
+            className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 pointer-events-none"
+          />
+        </div>
+        <div className={"relative"}>
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name={"password"}
+            placeholder={"Password"}/>
+          <img
+            src="/login/key.png"
+            alt="email"
+            aria-hidden="true"
+            className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 pointer-events-none"
+          />
+          <img
+            src="/login/view-off.png"
+            alt="email"
+            aria-hidden="true"
+            className="absolute right-4 w-5 top-1/2 -translate-y-1/2 pointer-events-none"
+          />
+        </div>
+        <a href={"#"} className={"text-slate-600 text-[13px] text-right"}>Şifremi Unuttum</a>
+        {error && <p className={"text-red-500"}>{error}</p>}
+      </div>
+      <button
+        className={"bg-primary py-3 px-8 rounded-2xl font-semibold cursor-pointer h-11"}
+        type="submit">
+        {loading ?
+          <img src="/login/loading.png" width={24} alt="loading" className={"animate-spin mx-auto"}/> :
+          "Giriş Yap"}
+      </button>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Input from "@/components/ui/input";
+import LoginForm from "@/app/login/login-form";
 
 export default function Page() {
   return (
@@ -14,52 +15,15 @@ export default function Page() {
           <h1 className={"font-semibold text-[32px] text-slate-900 max-w-[816px] text-center mx-auto"}>Reseller’a Hoş Geldin</h1>
           <h2 className={"text-slate-600 max-w-[816px] text-center mx-auto mt-4"}>Hesabınız ile oturum açın.</h2>
         </div>
-        <form className={"mx-auto max-w-[480px] mt-[72px] grid gap-8"}>
-          <div className={"grid gap-4"}>
-            <div className={"relative"}>
-              <Input
-                type="text"
-                name={"email"}
-                placeholder={"Email"}/>
-              <img
-                src="/login/email.png"
-                alt="email"
-                aria-hidden="true"
-                className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 pointer-events-none"
-              />
-            </div>
-            <div className={"relative"}>
-              <Input
-                type="text"
-                name={"password"}
-                placeholder={"Password"}/>
-              <img
-                src="/login/key.png"
-                alt="email"
-                aria-hidden="true"
-                className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 pointer-events-none"
-              />
-              <img
-                src="/login/view-off.png"
-                alt="email"
-                aria-hidden="true"
-                className="absolute right-4 w-5 top-1/2 -translate-y-1/2 pointer-events-none"
-              />
-            </div>
-            <a href={"#"} className={"text-slate-600 text-[13px] text-right"}>Şifremi Unuttum</a>
-          </div>
-          <button
-            className={"bg-primary py-3 px-8 rounded-2xl font-semibold cursor-pointer"}
-            type="submit">
-            Giriş Yap
-          </button>
+        <div className={"mx-auto max-w-[480px] mt-[72px] grid gap-8"}>
+          <LoginForm />
           <p className={"text-slate-600 text-[13px] mx-auto"}>
             Bir hesabın yok mu? &nbsp;
             <a className={"text-slate-900"} href="#">
               Yeni hesap oluştur
             </a>
           </p>
-        </form>
+        </div>
       </main>
 
       <footer className={"flex gap-7 items-center justify-center mt-[183px] text-[13px]"}>
