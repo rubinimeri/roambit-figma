a0e50fbf6aec9d4fba425ae195d2b3d6afcbcdcb
Rubin Imeri <rubinimeri6@gmail.com>
2025-09-28 23:53:49 +0200

    Change font weight & spacing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,13 +11,13 @@ export default function Page() {
 
       <main>
         <div className={"mt-[144px]"}>
-          <h1 className={"font-semibold text-[32px] text-slate-900 max-w-[816px] text-center mx-auto"}>Reseller’a Hoş Geldin</h1>
+          <h1 className={"font-medium text-[32px] text-slate-900 max-w-[816px] text-center mx-auto"}>Reseller’a Hoş Geldin</h1>
           <h2 className={"text-slate-600 max-w-[816px] text-center mx-auto mt-4"}>Hesabınız ile oturum açın.</h2>
         </div>
         <div className={"mx-auto max-w-[480px] mt-[72px] grid gap-8"}>
           <LoginForm />
-          <p className={"text-slate-600 text-[13px] mx-auto"}>
-            Bir hesabın yok mu? &nbsp;
+          <p className={"text-[13px] mx-auto flex gap-3"}>
+            <span className={"text-slate-600"}>Bir hesabın yok mu?</span>
             <a className={"text-slate-900"} href="#">
               Yeni hesap oluştur
             </a>
@@ -25,7 +25,7 @@ export default function Page() {
         </div>
       </main>
 
-      <footer className={"flex gap-7 items-center justify-center mt-[183px] text-[13px]"}>
+      <footer className={"flex gap-7 items-center justify-center mt-[257px] mb-[19px] text-[13px]"}>
         <div className={"flex gap-3"}>
           <img src="/login/flag.png" alt="flag" className={"max-w-6 h-[18px] self-center"}/>
           <p className={"flex gap-1 cursor-pointer"}>
